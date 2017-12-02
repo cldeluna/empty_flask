1317bdf430086ea1fa2e6a196e5fcdebc78659bf
Claudia de Luna <cldeluna@yahoo.com>
2017-12-02 13:37:36 -0800

    Adding jinja templates

--- server.py
+++ server.py
@@ -31,9 +31,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
 
   feed = feedparser.parse(RSS_FEEDS[publication])
-  first_article = feed['entries'][0]
 
-  return render_template("home.html", article=first_article)
+  return render_template("home.html", article=feed['entries'])
 
 if __name__ == "__main__":
     host = os.getenv('IP','0.0.0.0')
