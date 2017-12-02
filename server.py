f8bca877851eab8292a7f58e600637c758206db0
Claudia de Luna <cldeluna@yahoo.com>
2017-12-02 13:42:51 -0800

    Adding jinja templates

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'ansible': 'http://news.ansible.uk/rss.xml'}
+             'ansible': 'https://www.ansible.com/blog/rss.xml'}
 
 @app.route("/")
 @app.route("/<publication>")
