a8a8957f3196e0731c5749d98b4dca4925ee9a58
Claudia de Luna <cldeluna@yahoo.com>
2017-12-02 12:53:36 -0800

    added ansible rss feed

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'ans': 'http://news.ansible.uk/rss.xml'}
+             'ansible': 'http://news.ansible.uk/rss.xml'}
 
 @app.route("/")
 @app.route("/<publication>")
